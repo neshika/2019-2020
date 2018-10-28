7b53c2f4a8c3f850f66598e5d273c7c18a85a29c
neshika <stepanovaml@mail.ru>
2018-10-29 00:37:12 +0300

    - 28.10.2018 - 90. отрисовываем breeding.php

--- breedding.php
+++ breedding.php
@@ -14,6 +14,38 @@ error_reporting(E_ALL);
 </head>
 <body>
 
+  <style>
+      body,html {
+          width: 100%;
+          height: 100%;
+      }
+      .wrapper{
+            margin-left: 15%; /* Отступ слева */
+            width: 1250px; /* Ширина слоя */
+            height: auto;
+            background: #D0D0D0; /* Цвет фона */
+            border: 1px groove #9868CE;
+            padding: 10px; /* Поля вокруг текста */
+          
+      }
+  
+   TD {
+    vertical-align: top; /* Выравнивание по верхнему краю ячейки */
+   }
+   #col1 {
+   
+    background: red; /* Цвет фона первой колонки */
+   }
+   #col2 {
+   
+    background: blueviolet; /* Цвет фона второй колонки */
+   }
+   #col3 {
+    
+    background: yellowgreen; /* Цвет фона третьей колонки */
+   }
+     
+  </style>
  <?php
  $temp=(int)$_SESSION['para'];
 $temp2=(int)$_POST['ONONA'];
@@ -32,41 +64,44 @@ $temp2=(int)$_POST['ONONA'];
  
 // ******************** вывод картинки мамы и папы по id  из базы *****************-->  
 ?>  
-<table>
-    <colgroup>
-        <col span='2' style="background: khaki"></colgroup>
-    <caption><h1>Вяжем двух собак</h1></caption>
-        <tr>
-            <th><h3>Мама: <?php echo $id_m;
-                            dog_pic($id_m);
-                            detalis($id_m);
-                            f_tree($id_m);    ?>
-                    
-                </h3>
-            <th>
-                <h3>Папа:<?php echo $id_d;
-                            dog_pic($id_d);
-                            detalis($id_d);
-                            f_tree($id_d);?>
+    <div class="wrapper"><h1 align="center"> Вяжем двух собак</h1>
+        
+        <table width="100%" cellpadding="5" cellspacing="0">
+  <tr>
+  
+ 
+         <td id="col1"><h3 align="center">Мама: </h3>
+      
+           <?php echo $id_m;
+           dog_pic($id_m);
+           detalis($id_m);
+           f_tree($id_m);    ?>
+
+       </td>
+        <td id="col2"><h3 align="center">Особенности: </h3>
+                    <?php if(bdika_balance($_SESSION['owner'],5000)){ //проверка остатка средств на вязку. если хватает активна кнопка "ВЯЗКА" ?>
+                            <form method="POST" action="/NewDog.php">
+                                <input type="submit" name="nazvanie_knopki" value="Вяжем" class="knopka"/>
+                            </form>
+                          <?php }else 
+                                echo 'не достаточно средст для вязки!';
+
+                            ?>
+                            <form method="POST" action="/kennel.php">
+                                <input type="submit" name="exit" value="Вернуться" class="knopka"/>
+                            </form>
                  </h3>
-            </th>
-            <th>
-                <?php if(bdika_balance($_SESSION['owner'],5000)){ //проверка остатка средств на вязку. если хватает активна кнопка "ВЯЗКА" ?>
-            <form method="POST" action="/NewDog.php">
-                <input type="submit" name="nazvanie_knopki" value="Вяжем" class="knopka"/>
-            </form>
-<?php }else 
-      echo 'не достаточно средст для вязки!';
-
-?>
-
-<form method="POST" action="/kennel.php">
-    <input type="submit" name="exit" value="Вернуться" class="knopka"/>
-</form>
-            </th>
-              
-        </tr>
-</table>
+        </td>
+        <td id="col3"><h3 align="center">Папа:</h3>
+                <?php echo $id_d;
+               dog_pic($id_d);
+               detalis($id_d);
+               f_tree($id_d);?>
+             
+        </td>
+   </tr>
+  </table>
+    </div>        
  <?php 
 $_SESSION['id_m']=$id_m;
 $_SESSION['id_d']=$id_d;
