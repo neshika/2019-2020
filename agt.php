2781ace919d568a624af137d6bcd60ba922551ed
neshika <stepanovaml@mail.ru>
2020-06-10 08:41:55 +0300

    10/06/20 изменение основного шрифта на Montserrat

--- agt.php
+++ agt.php
@@ -4,176 +4,6 @@ require __DIR__."/db.php";
 require __DIR__."/includes/functions.php";
 //require __DIR__."/mumdad.php";
 ?>
-
-<!DOCTYPE html> 
-<html>
-<head>
-<meta http-equiv='Content-Type' content='text/html; charset=utf8'>
-
-<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script> 
-<script type="text/javascript" src="js/script.js"></script> <img src=""/>
-<link rel="stylesheet" href="/css/styleagt.css" type="text/css" />
-<link rel="stylesheet" media="screen and (max-width: 600px)" href="/css/small.css" type="text/css" />
-
-
-
-<title>Cимулятор заводчика</title>
-</head>
-<body bgcolor="#22252F" body text="#CABDCF">
-<?php require __DIR__ . "/html/navagt.html";// вставляем шапку АГТ?>
- 
-    <p>Абзац с текстом был добавлен для демонстрации того, что при открытии подпунктов меню они будут скрывать часть контента, а не сдвигать его.</p>
-<marquee behavior="alternate" direction="right" bgcolor="#5E3561"><?php echo date('d.m.Y : h.m');?></marquee> 
- 
-<style>
-      table {
-        table-layout: fixed;
-        text-align: left;
-        border-collapse: separate;
-        border-spacing: 5px;
-        background: #CABDCF;
-        border-radius: 20px;
-        }
-        .table1{
-            background: #21252E;
-            border-radius: 20px;
-            vertical-align:top;            
-        }
-        
-        th{
-        font-size: 18px;
-        padding: 10px;
-        }
-        td{
-        background: #21252E;
-        padding: 10px;
-        }
-       
-        caption{
-            caption-side: top;
-        }
-     </style>         
-<form method="POST" action="/agt.php">     
- <input type="submit" class="knopka2" value="Обновить" onClick="window.location.reload( true );">
- <button type="submit" class="knopka2" name="buy">Обновить родителей</button>
- 
-</form>
-<?php
-//если нажали кнопку "обновить родителей"
-if ( isset($_POST['buy']) ){
-        //данные мамы
-    $id='1';
-    $dog_id='1';
-    $a_mum= f_rand_col('AA', 'Aa', 'aa');
-     $b_mum= f_rand_col('BB', 'Bb', 'bb');
-     $c_mum= f_rand_col('CC', 'CC', 'CC');
-    $d_mum= f_rand_col('DD', 'Dd', 'dd');
-    $p_mum= f_rand_col('PP', 'Pp', 'pp');
-    $m_mum= f_rand_col('mm', 'mm', 'mm');
-    $t_mum='tt';
-    $sex='0';
-    R::exec( 'UPDATE dna_agt SET id=:dog_id WHERE id = :id ', array(':dog_id'=> $dog_id, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET a=:speed WHERE id = :id ', array(':speed'=> $a_mum, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET b=:agility WHERE id = :id ', array(':agility'=> $b_mum, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET c=:teach WHERE id = :id ', array(':teach'=> $c_mum, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET d=:jump WHERE id = :id ', array(':jump'=> $d_mum, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET p=:scent WHERE id = :id ', array(':scent'=> $p_mum, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET m=:find WHERE id = :id ', array(':find'=> $m_mum, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET t=:total WHERE id = :id ', array(':total'=> $t_mum, ':id' => $id));
-    R::exec( 'UPDATE dna_agt SET sex=:total WHERE id = :id ', array(':total'=> $sex, ':id' => $id));
-
-
-    //данные папы:
-    $id2='2';
-    $dog_id2='2';
-    $a_dad= f_rand_col('AA', 'Aa', 'aa');
-    $b_dad= f_rand_col('BB', 'Bb', 'bb');
-    $c_dad= f_rand_col('CC', 'CC', 'CC');
-    $d_dad= f_rand_col('DD', 'Dd', 'dd');
-    $p_dad= f_rand_col('PP', 'Pp', 'pp');
-    $m_dad= f_rand_col('mm', 'mm', 'mm');
-    $t_dad='tt';
-    $sex2='1';
-    R::exec( 'UPDATE dna_agt SET id=:dog_id WHERE id = :id ', array(':dog_id'=> $dog_id2, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET a=:speed WHERE id = :id ', array(':speed'=> $a_dad, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET b=:agility WHERE id = :id ', array(':agility'=> $b_dad, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET c=:teach WHERE id = :id ', array(':teach'=> $c_dad, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET d=:jump WHERE id = :id ', array(':jump'=> $d_dad, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET p=:scent WHERE id = :id ', array(':scent'=> $p_dad, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET m=:find WHERE id = :id ', array(':find'=> $m_dad, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET t=:total WHERE id = :id ', array(':total'=> $t_dad, ':id' => $id2));
-    R::exec( 'UPDATE dna_agt SET sex=:total WHERE id = :id ', array(':total'=> $sex2, ':id' => $id2));
-
-
-}
-
-echo 'ТЕСТИМ выводим базу данных: DNA_AGT' . print_al();?>
-
-<table>
-    <tr>
-        <td>
-          
-            <table class="table1" cellpadding="2">2
-                <tr>
-                    <td valign="top" height="300" width="300">фото папы:<br>
-                        <?php 
-                        $id2='2';
-                        $tabl='dna_agt';
-                        $array2=take_data_from($id2, $tabl);
-                        $put= 'pici/agt/' . bdika_col_dog($array2['a'][0],$array2['b'][0],$array2['c'][0],$array2['d'][0],$array2['p'][0],$array2['m'][0]). '.png';
-                        ?><img src="<?php echo $put;?>">
-                    </td>
-                    <td valign="top" rowspan="2">данные собаки левый столбец<br><br>
-                        <li>Генетический код: <?php echo $array2['a'] . $array2['b'] . $array2['c'] . $array2['d'] . $array2['p'] . $array2['m'];?></li>
-                        <li>ОКРАС: <?php echo bdika_col_dog2($array2['a'][0],$array2['b'][0],$array2['c'][0],$array2['d'][0],$array2['p'][0],$array2['m'][0]);?></li>
-                        <li>Цвет глаз: <?php echo print_col_ears_ru($array2['p'],$array2['d'],$array2['c'],$array2['a']);?></li>
-                        <li>Пол: кабель</li>
-                    </td>
-                </tr>
-                <tr>
-                    <td>ГК: <?php echo do_gencode($array2['a'][0],$array2['b'][0],$array2['c'][0],$array2['d'][0],$array2['p'][0],$array2['m'][0]);?>
-                    <br>Цвет глаз: <br><?php echo print_col_ears($array2['p'],$array2['d'],$array2['c'],$array2['a']) .print_col_ears($array2['p'],$array2['d'],$array2['c'],$array2['a']);?>
-                    </td>
-                </tr>
-            </table>
-    </td>
-    <td>
-        <table class="table1" cellpadding="2">1 
-          
-      <tr>
-        <td valign="top" height="300" width="300">фото мамы: <br>
-             <?php
-             $id='1';
-                $tabl='dna_agt';
-                $array=take_data_from($id, $tabl);
-             $put2= 'pici/agt/' . bdika_col_dog($array['a'][0],$array['b'][0],$array['c'][0],$array['d'][0],$array['p'][0],$array['m'][0]). '.png';
-                ?><img src="<?php echo $put2;?>">
-        </td>
-        <td valign="top" rowspan="2">данные собаки правый столбец<br><br>
-            <li>Генетический код: <?php echo $array['a'] . $array['b'] . $array['c'] . $array['d'] . $array['p'] . $array['m'];?></li>
-            <li>ОКРАС: <?php echo bdika_col_dog2($array['a'][0],$array['b'][0],$array['c'][0],$array['d'][0],$array['p'][0],$array['m'][0]);?></li>
-            <li>Цвет глаз: <?php echo print_col_ears_ru($array['p'],$array['d'],$array['c'],$array['a']);?></li>
-            <li>Пол: сука</li>
-        </td>
-      </tr>
-      <tr>
-        <td>ГК: <?php echo do_gencode($array['a'][0],$array['b'][0],$array['c'][0],$array['d'][0],$array['p'][0],$array['m'][0]);?>
-        <br>Цвет глаз: <br><?php echo print_col_ears($array['p'],$array['d'],$array['c'],$array['a']) .print_col_ears($array['p'],$array['d'],$array['c'],$array['a']);?>
-        </td>
-      </tr>
-         
-      </table>
-            
-            
-        </td>
-    </tr>
-    
-</table>
-
-
-
-
 <?php
 
 
@@ -739,6 +569,148 @@ function print_al(){
               echo "<br><br>";
             }
 }
+//если нажали кнопку "обновить родителей"
+if ( isset($_POST['buy']) ){
+        //данные мамы
+    $id='1';
+    $dog_id='1';
+    $a_mum= f_rand_col('AA', 'Aa', 'aa');
+     $b_mum= f_rand_col('BB', 'Bb', 'bb');
+     $c_mum= f_rand_col('CC', 'CC', 'CC');
+    $d_mum= f_rand_col('DD', 'Dd', 'dd');
+    $p_mum= f_rand_col('PP', 'Pp', 'pp');
+    $m_mum= f_rand_col('mm', 'mm', 'mm');
+    $t_mum='tt';
+    $sex='0';
+    R::exec( 'UPDATE dna_agt SET id=:dog_id WHERE id = :id ', array(':dog_id'=> $dog_id, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET a=:speed WHERE id = :id ', array(':speed'=> $a_mum, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET b=:agility WHERE id = :id ', array(':agility'=> $b_mum, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET c=:teach WHERE id = :id ', array(':teach'=> $c_mum, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET d=:jump WHERE id = :id ', array(':jump'=> $d_mum, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET p=:scent WHERE id = :id ', array(':scent'=> $p_mum, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET m=:find WHERE id = :id ', array(':find'=> $m_mum, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET t=:total WHERE id = :id ', array(':total'=> $t_mum, ':id' => $id));
+    R::exec( 'UPDATE dna_agt SET sex=:total WHERE id = :id ', array(':total'=> $sex, ':id' => $id));
+
+
+    //данные папы:
+    $id2='2';
+    $dog_id2='2';
+    $a_dad= f_rand_col('AA', 'Aa', 'aa');
+    $b_dad= f_rand_col('BB', 'Bb', 'bb');
+    $c_dad= f_rand_col('CC', 'CC', 'CC');
+    $d_dad= f_rand_col('DD', 'Dd', 'dd');
+    $p_dad= f_rand_col('PP', 'Pp', 'pp');
+    $m_dad= f_rand_col('mm', 'mm', 'mm');
+    $t_dad='tt';
+    $sex2='1';
+    R::exec( 'UPDATE dna_agt SET id=:dog_id WHERE id = :id ', array(':dog_id'=> $dog_id2, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET a=:speed WHERE id = :id ', array(':speed'=> $a_dad, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET b=:agility WHERE id = :id ', array(':agility'=> $b_dad, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET c=:teach WHERE id = :id ', array(':teach'=> $c_dad, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET d=:jump WHERE id = :id ', array(':jump'=> $d_dad, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET p=:scent WHERE id = :id ', array(':scent'=> $p_dad, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET m=:find WHERE id = :id ', array(':find'=> $m_dad, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET t=:total WHERE id = :id ', array(':total'=> $t_dad, ':id' => $id2));
+    R::exec( 'UPDATE dna_agt SET sex=:total WHERE id = :id ', array(':total'=> $sex2, ':id' => $id2));
+
+
+}
+
 ?>
-    
+<!DOCTYPE html> 
+<html>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf8'>
+
+<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script> 
+<script type="text/javascript" src="js/script.js"></script> <img src=""/>
+<link rel="stylesheet" href="/css/styleagt.css" type="text/css" />
+<link rel="stylesheet" media="screen and (max-width: 600px)" href="/css/small.css" type="text/css" />
+
+<title>Cимулятор заводчика</title>
+</head>
+<body bgcolor="#22252F" body text="#CABDCF">
+<?php require __DIR__ . "/html/navagt.html";// вставляем шапку АГТ?>
+ 
+    <p>Абзац с текстом был добавлен для демонстрации того, что при открытии подпунктов меню они будут скрывать часть контента, а не сдвигать его.</p>
+<marquee behavior="alternate" direction="right" bgcolor="#5E3561"><?php echo date('d.m.Y : h.m');?></marquee> 
+ 
+<style>
+      table {
+        table-layout: fixed;
+        text-align: left;
+        border-collapse: separate;
+        border-spacing: 5px;
+        background: #CABDCF;
+        border-radius: 20px;
+        }
+        .table1{
+            background: #21252E;
+            border-radius: 20px;
+            vertical-align:top;            
+        }
+        
+        th{
+        font-size: 18px;
+        padding: 10px;
+        }
+        td{
+        background: #21252E;
+        padding: 10px;
+        }
+       
+        caption{
+            caption-side: top;
+        }
+     </style>         
+<form method="POST" action="/agt.php">     
+ <input type="submit" class="knopka2" value="Обновить" onClick="window.location.reload( true );">
+ <button type="submit" class="knopka2" name="buy">Обновить родителей</button>
+</form>
+<div>ТЕСТИМ выводим базу данных: DNA_AGT <br><br><?php print_al()?></div>
+<table><tr><td>
+       <table class="table1" cellpadding="2">2
+        <tr><td valign="top" height="300" width="300">фото папы:<br>
+            <?php $id2='2';
+                $tabl='dna_agt';
+                $array2=take_data_from($id2, $tabl);
+                $put= 'pici/agt/' . bdika_col_dog($array2['a'][0],$array2['b'][0],$array2['c'][0],$array2['d'][0],$array2['p'][0],$array2['m'][0]). '.png';
+                ?><img src="<?php echo $put;?>">
+            </td>
+            <td valign="top" rowspan="2">данные собаки левый столбец<br><br>
+               <li>Генетический код: <?php echo $array2['a'] . $array2['b'] . $array2['c'] . $array2['d'] . $array2['p'] . $array2['m'];?></li>
+                <li>ОКРАС: <?php echo bdika_col_dog2($array2['a'][0],$array2['b'][0],$array2['c'][0],$array2['d'][0],$array2['p'][0],$array2['m'][0]);?></li>
+                <li>Цвет глаз: <?php echo print_col_ears_ru($array2['p'],$array2['d'],$array2['c'],$array2['a']);?></li>
+                <li>Пол: кабель</li>
+            </td>
+        </tr>
+        <tr><td>ГК: <?php echo do_gencode($array2['a'][0],$array2['b'][0],$array2['c'][0],$array2['d'][0],$array2['p'][0],$array2['m'][0]);?>
+            <br>Цвет глаз: <br><?php echo print_col_ears($array2['p'],$array2['d'],$array2['c'],$array2['a']) .print_col_ears($array2['p'],$array2['d'],$array2['c'],$array2['a']);?>
+            </td>
+        </tr>
+      </table>
+    </td><td>
+      <table class="table1" cellpadding="2">1 
+         <tr><td valign="top" height="300" width="300">фото мамы: <br>
+            <?php $id='1';
+                $tabl='dna_agt';
+                $array=take_data_from($id, $tabl);
+                $put2= 'pici/agt/' . bdika_col_dog($array['a'][0],$array['b'][0],$array['c'][0],$array['d'][0],$array['p'][0],$array['m'][0]). '.png';
+                ?><img src="<?php echo $put2;?>">
+        </td><td valign="top" rowspan="2">данные собаки правый столбец<br><br>
+            <li>Генетический код: <?php echo $array['a'] . $array['b'] . $array['c'] . $array['d'] . $array['p'] . $array['m'];?></li>
+            <li>ОКРАС: <?php echo bdika_col_dog2($array['a'][0],$array['b'][0],$array['c'][0],$array['d'][0],$array['p'][0],$array['m'][0]);?></li>
+            <li>Цвет глаз: <?php echo print_col_ears_ru($array['p'],$array['d'],$array['c'],$array['a']);?></li>
+            <li>Пол: сука</li>
+        </td></tr>
+      <tr><td>ГК: <?php echo do_gencode($array['a'][0],$array['b'][0],$array['c'][0],$array['d'][0],$array['p'][0],$array['m'][0]);?>
+            <br>Цвет глаз: <br><?php echo print_col_ears($array['p'],$array['d'],$array['c'],$array['a']) .print_col_ears($array['p'],$array['d'],$array['c'],$array['a']);?>
+          </td></tr>
+         
+      </table>
+      </td></tr>
+</table>
+   
 </body>
