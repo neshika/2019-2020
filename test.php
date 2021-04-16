8c1af609cfe070827859aa909d9c8d280ac84123
neshika <neshika@192.168.1.8>
2021-04-17 00:42:11 +0300

    149 новые иконки на форме test

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 //подключение файлов
 require_once(__DIR__ . '/libs/up.php');
 require_once(__DIR__ . '/includes/func.php');
+
      
 ?>
 <style>
@@ -40,6 +41,7 @@ require_once(__DIR__ . '/includes/func.php');
         width: 100%;
         border-collapse:collapse;
         border-spacing:0
+            
     }
     table, td, th{
         border: 1px solid #595959;
@@ -87,12 +89,21 @@ require_once(__DIR__ . '/includes/func.php');
 ?>
    
 <div class="table-responsive"><table>
-	<tbody>
+       <tbody>
 		<tr class="stroka1">
                     <td class="текст"><div class="border_text">
-                   
-                        Кличка: <strong> <?php echo $dog->retName($id) . " " . $dog->retKennel($id) . $dog->picSex($id);?></strong>
+                        <button type="button" class="btn btn-dark">Есть <i class="fa fa-cutlery" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-dark">Пить <i class="fa fa-tint" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-dark">Чесать <i class="fa fa-bath" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-dark">Гулять <i class="fa fa-umbrella" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-dark">Спать <i class="fa fa-bed" aria-hidden="true"></i></button>
+                        <button type="button" class="btn btn-dark">Растить <i class="fa fa-line-chart" aria-hidden="true"></i></button><br>
+                        </div>
+                        <br>Кличка: <strong> <?php echo $dog->retName($id) . " " . $dog->retKennel($id) . $dog->picSex($id);?></strong>
                                 <hr>
+                            
+
+
                        <li>Заводчик: <?php echo $dog->retBreeder($id);?></li>    
                        <li>Хозяин: <?php echo $owner;?></li>
                        <li>Происхождение: <?php echo $dog->retOrignText($id);?></li>
@@ -104,36 +115,46 @@ require_once(__DIR__ . '/includes/func.php');
                        <li>Возраст: <?php echo $dog->retAgeText($id);?></li>
                        <li>Вязок: <?php echo $dog->retLitter($id);?></li>
                        <li>Щенков: <?php echo $dog->retPuppy($id);?></li>
-                       <li>Состояние:  <?php echo $dog->retEstrusText($id);?></li></div>
+                       <li>Состояние:  <?php echo $dog->retEstrusText($id);?></li>
+                       <li>Голая/пуховая:  </li>
+                       <li>Шокоген:  </li>
+                       <a href="#" class="btn btn btn-dark" role="button" aria-pressed="true">Ссылка</a><br>
+                      
                      </td>
 			<td class="пусто"></td>
 			<td class="картинка"><div class="border_pic"> <?php $dog->picLink($id, 220);?>
+                            
+                            </div><button type="button" class="btn btn-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="Добавки +Энергия"><i class="fa fa-leaf fa-2x" aria-hidden="true"></i><i class="fa fa-bolt" aria-hidden="true"></i>+</button>
+                        <button type="button" class="btn btn-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="Спа уход +Счастье" ><i class="fa fa-umbrella fa-2x" aria-hidden="true"></i> <i class="fa fa-certificate" aria-hidden="true"></i>+</button>
+                        <button type="button" class="btn btn-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="Ветеринар +Здоровье"><i class="fa fa-medkit fa-2x" aria-hidden="true"></i> <i class="fa fa-heart" aria-hidden="true"></i>+</button>
+                        <button type="button" class="btn btn-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="Тренировка -Энергия+Счастье"><i class="fa fa-graduation-cap fa-2x" aria-hidden="true"></i> <i class="fa fa-bolt" aria-hidden="true"></i>-<i class="fa fa-certificate" aria-hidden="true"></i>+</button>
+                        <br></td>
                              
-                            </div></td>
 		</tr>
 		<tr class="stroka2">
 			<td class="пусто"></td>
-			<td class="статус бары" colspan="2"><li>Генетический код: <?php echo $dna->retDna($dna_id);?></li>
-                        <button type="button" class="btn btn-dark">Темный</button>
-                             <a href="#" class="btn btn btn-dark" role="button" aria-pressed="true">Ссылка</a>
+			<td class="статус бары" colspan="2">
+                <li>Генетический код: <?php echo $dna->retDna($dna_id);?></li>
+                      
                              <table width="100%">
                                 <tbody>
                                     <tr>
-                                            <td>Энергия</td>
-                                            <td width="75%"><div class="progress">
+                                            <td><i class="fa fa-tachometer" aria-hidden="true"></i> Энергия</td>
+
+                                            <td width="66%"><div class="progress">
                                                     <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $dog->retVitality($id) . '%';?>" aria-valuenow="75" aria-valuemin="0"                                                      aria-valuemax="100"><?php echo $dog->retVitality($id);?></div>
                                             </div></td>
                                             
                                     </tr>
                                     <tr>
-                                            <td>Счастье</td>
+                                            <td><i class="fa fa-sun-o" aria-hidden="true"></i> Счастье</td>
                                             <td><div class="progress">
                                                 <div class="progress-bar bg-warning" role="progressbar" style="width: <?php echo $dog->retJoy($id) . '%';?>" aria-valuenow="75" aria-valuemin="0"                                                      aria-valuemax="100"><?php echo $dog->retJoy($id);?></div>
                                           </div></td>
                                           
                                     </tr>
                                      <tr>
-                                            <td>Здоровье</td>
+                                            <td><i class="fa fa-heartbeat" aria-hidden="true"></i> Здоровье</td>
                                             <td><div class="progress">
                                                     <div class="progress-bar bg-danger" role="progressbar" style="width: <?php echo $dog->retHP($id) . '%';?>" aria-valuenow="75" aria-valuemin="0"                                                      aria-valuemax="100"><?php echo $dog->retHp($id);?></div>
                                           </div></td>
@@ -145,6 +166,7 @@ require_once(__DIR__ . '/includes/func.php');
 		</tr>
 	</tbody>
 </table></div>
-
-    
+</div>    
+<?php 
+require_once(__DIR__ . '/html/footer.html');    
  
