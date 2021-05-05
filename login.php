7942a29355e423195aa7563ad179c4b66e808128
neshika <neshika@192.168.1.8>
2021-05-06 00:08:44 +0300

    150 кнопка "изменить пароль"/сделать неактивной disabled="disabled"

--- login.php
+++ login.php
@@ -1,17 +1,9 @@
 <?php
 require "db.php";
-$data = $_POST;
 
-if( isset($data['lo_zoher']) ){
-    echo "Введите новый пароль.";
-    $errors = array();
-    if(''==trim($data['login'])){	//TRIM убирает все пробелы Если логин пустой
-	$errors [] = 'поле логина не заполнено';
-    }
-    if(''==($data['password'])){	// Если Пароль пустой   ПАРОЛЬ НЕ ТРИМАЕМ
-	$errors [] = 'поле  password не заполнено';
-    }
-}
+$data = $_POST;
+//var_dump($data);
+
 if( isset($data['do_login']) ){
 	$errors= array();
 	$user = R::findOne ('users','login = ?', array($data['login']));
@@ -43,7 +35,7 @@ if( isset($data['do_login']) ){
 		}
 
 	}else{
-		$errors [] = 'Пользоватеь не найден ' ;
+		$errors [] = 'Пользователь не найден ' ;
 
 	}	
 
@@ -54,6 +46,30 @@ if( isset($data['do_login']) ){
 		echo '<div style="color:red;">'.array_shift($errors).'</div><hr>';
 	}
 
+}
+if( isset($data['lo_zoher']) ){
+    require_once(__DIR__ . '/includes/func.php');
+   // echo "Введите новый пароль.";
+    $errors = array();
+    if(''==trim($data['login'])){	//TRIM убирает все пробелы Если логин пустой
+	$errors [] = 'поле логина не заполнено';
+    }
+    if(''==($data['password'])){	// Если Пароль пустой   ПАРОЛЬ НЕ ТРИМАЕМ
+	$errors [] = 'поле  password не заполнено';
+    }
+    if (empty($errors)){
+		//все хорошо, регистрируем 
+		/*  создаем базу через REdBeen*/
+		echo "меняем пароль";
+                $owner_id = new Users();
+                $id = $owner_id->retId($data ['login']);
+                $user = R::load('users', $id);
+		$user->password = password_hash($data ['password'], PASSWORD_DEFAULT); //зашифровываем пароль
+		R::store ($user);
+		echo '<div style="color:green;">Пароль успешно изменен</div><hr>';
+    } else{
+		echo '<div style="color:red;">'.array_shift($errors).'</div><hr>';
+    }
 }
 
 require_once(__DIR__ . '/html/login.html');
