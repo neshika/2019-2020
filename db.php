1b4ba7f3779cd344c15f7f062d95e65dbb888f9c
neshika <neshika@192.168.1.8>
2021-07-06 12:17:25 +0300

    изменен пароль в БД 321

--- db.php
+++ db.php
@@ -2,8 +2,8 @@
 //файл db.php
 
 require "libs/rb.php";
-R::setup('mysql:host=127.0.0.1;dbname=My_bd',
-        'root', 'root' ); //for both mysql or mariaDB
+R::setup('mysql:host=127.0.0.1;dbname=my_bd',
+        'root', '321478828' ); //for both mysql or mariaDB
 //R::setup('mysql:host=localhost;dbname=id8694121_my_bd',
   //    'id8694121_name', 'dg(pEz4qUSCZRS4' );
 session_start();
