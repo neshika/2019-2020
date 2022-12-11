f991d13f8ac7b9f42849d2ec01ac8ee5fd043693
neshika <stepanovaml@mail.ru>
2022-12-11 21:25:00 +0300

    доступ к базе восстановлен

--- db.php
+++ db.php
@@ -4,21 +4,19 @@
 require 'libs/rb.php';
 
 // локальное подключение
-if ($_SERVER['HTTP_HOST'] == 'localhost')
- {
+if ($_SERVER['HTTP_HOST'] == 'dog.ru') {
   R::setup(
     'mysql:host=127.0.0.1;dbname=my_bd',
     'root',
     '321478828'
-  ); 
-  } else
-   {
+  );
+} else {
   // db www.neshika.ru
   R::setup(
     'mysql:host=localhost;dbname=ca33905_dog',
     'ca33905_dog',
     'Z321478828z'
   );
-  }
-  session_start();
+}
+session_start();
   //echo 'db включена';
